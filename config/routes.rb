227a0a78e3eaa7b9f08f35f548a6d13fca4ea85c
Gary Taylor <gary.taylor@hismessages.com>
2018-03-13 08:57:11 +0000

    Added initial route for the new claim

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :jadu do
+    post 'new-claim' => 'claims#create'
+  end
 end
