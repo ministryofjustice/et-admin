e74fb80eaed4a234f9b5024080ebd69c689c96a2
Gary Taylor <gary.taylor@hismessages.com>
2018-04-09 13:16:03 +0100

    Incorrectly used permissions_names instead of permission_names in routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ require 'sidekiq/cron/web'
 Rails.application.routes.draw do
   devise_for :'admin/users', ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  authenticate :admin_user, -> (u) { u.is_admin? || u.permissions_names.include?('read_jobs') } do |u|
+  authenticate :admin_user, -> (u) { u.is_admin? || u.permission_names.include?('read_jobs') } do |u|
     mount Sidekiq::Web => '/admin/sidekiq'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
