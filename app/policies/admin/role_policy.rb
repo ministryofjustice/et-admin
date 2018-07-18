eb9fac34290b37e4e8eba72613f8a3cb1d823a7b
Gary Taylor <gary.taylor@hismessages.com>
2018-07-18 21:04:48 +0100

    Roles can now only be updated by an administrator

--- app/policies/admin/role_policy.rb
+++ app/policies/admin/role_policy.rb
@@ -7,14 +7,18 @@ module Admin
     end
 
     def show?
-      true
+      user.is_admin?
     end
 
     def index?
-      true
+      user.is_admin?
     end
 
     def update?
+      user.is_admin?
+    end
+
+    def delete?
       user.is_admin?
     end
   end
