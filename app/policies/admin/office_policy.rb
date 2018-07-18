d8f05c3599237674fc731e89ba2beeeaf2c68c04
Gary Taylor <gary.taylor@hismessages.com>
2018-07-18 20:56:29 +0100

    Edit, delete and list offices are now under control of the roles and permissions

--- app/policies/admin/office_policy.rb
+++ app/policies/admin/office_policy.rb
@@ -1,11 +1,20 @@
 module Admin
   class OfficePolicy < ApplicationPolicy
     def index?
-      true
+      user.is_admin? || user.permission_names.include?('read_offices')
     end
 
     def show?
-      false
+      user.is_admin? || user.permission_names.include?('read_offices')
     end
+
+    def update?
+      user.is_admin? || user.permission_names.include?('update_offices')
+    end
+
+    def destroy
+      user.is_admin? || user.permission_names.include?('delete_offices')
+    end
+
   end
 end
