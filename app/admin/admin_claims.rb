7259d857ce752e1d15e078787b955626e42575f3
Efstathios Stivaros <efstathiosstivaros@gmail.com>
2018-11-06 15:14:59 +0000

    tempcommit

--- app/admin/admin_claims.rb
+++ app/admin/admin_claims.rb
@@ -1,5 +1,49 @@
 ActiveAdmin.register Claim, as: 'Claims' do
-# See permitted parameters documentation:
+#   preserve_default_filters!
+#   filter :claim_claimants, label: 'Secondary Claimants', collection: proc {
+#     ClaimClaimant.all.map { |claim| claim.claimant.name }
+#   }
+#   filter :claim_respondents, collection: proc {
+#     ClaimRespondent.all.map { |claim| claim.respondent.name }
+#   }
+#   filter :claim_representatives, collection: proc {
+#     ClaimRepresentative.all.map { |claim| claim.representative.name}
+#   }
+#   filter :claim_uploaded_files, collection: proc {
+#     ClaimUploadedFile.all.map { |claim| claim.uploaded_file.filename}
+#   }
+#   remove_filter :claimants
+#   # filter :respondents
+#   # filter :representatives
+#   filter :uploaded_files, collection: proc {
+#     UploadedFile.all.map { |file| file.filename}
+#   }
+#   # filter :primary_claimant
+#   # filter :reference_contains
+#   # filter :submission_reference_contains
+#   filter :submission_channel, as: :check_boxes, collection: proc {
+#     Claim.all.map(&:submission_channel).uniq
+#   }
+#   filter :case_type, as: :check_boxes, collection: proc {
+#     Claim.all.map(&:case_type).uniq
+#   }
+#   filter :jurisdiction, as: :check_boxes, collection: proc { Claim.all.map(&:jurisdiction).uniq}
+#   # filter :office_code_equals
+#   # filter :name_of_recipient
+#   remove_filter :administrator
+#   # filter :created_at
+#   # filter :updated_at
+#   # filter :other_known_claimant_names
+#   filter :discrimination_claims, as: :check_boxes, collection: proc { Claim.all.map(&:discrimination_claims).flatten.uniq }
+#   # filter :pay_claims
+#   # filter :desired_outcomes
+#   # filter :other_claim_details
+#   # filter :claim_details
+#   # filter :other_outcome
+#   filter :send_claim_to_whistleblowing_entity, as: :check_boxes
+#   # filter :miscellaneous_information
+#   filter :is_unfair_dismissal, as: :check_boxes
+# # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 # permit_params :list, :of, :attributes, :on, :model
