ce730221a306b92cde48e6fa06afec8f7e9456d3
garytaylor <gary.taylor@hismessages.com>
2019-05-15 10:25:14 +0100

    SSL Certificate Verification Control

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -104,5 +104,6 @@ Rails.application.configure do
 
   Raven.configure do |config|
     config.dsn = ENV.fetch('RAVEN_DSN', '')
+    config.ssl_verification = ENV.fetch('RAVEN_SSL_VERIFICATION', 'true').downcase == 'true'
   end
 end
