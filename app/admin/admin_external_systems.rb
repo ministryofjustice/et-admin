5a2f09dfec7affdde704b2a138dc511f2800399b
Gary Taylor <gary.taylor@hismessages.com>
2019-06-21 14:48:05 +0100

    RST-1930 Added extra fields for exporter code

--- app/admin/admin_external_systems.rb
+++ app/admin/admin_external_systems.rb
@@ -3,7 +3,7 @@ ActiveAdmin.register ExternalSystem, as: 'External Systems' do
   filter :name
   filter :reference
 
-  permit_params :name, :reference, :enabled, office_codes: [], configurations_attributes: [:id, :key, :value, :_destroy]
+  permit_params :name, :reference, :enabled, :export, :export_queue, office_codes: [], configurations_attributes: [:id, :key, :value, :_destroy]
 
   show do |system|
     default_attribute_table_rows = active_admin_config.resource_columns
